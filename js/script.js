02f5809fe9df1fe7edec403bd4bd2636c1a587c8
Christoph Szczechowicz <christoph.szczechowicz@gmail.com>
2016-05-03 23:22:26 -0400

    Removed border while scrolling to the main area

--- js/script.js
+++ js/script.js
@@ -4,10 +4,10 @@ $(window).scroll(function() {
     windowHeight = $(window).height(),
     windowScroll = $(this).scrollTop();
   if ( windowScroll < (aboutScroll + margin - windowHeight)){
-    $(".navbar-inverse").css("background-color", "transparent");
+    $(".navbar-inverse").css({"background-color": "transparent" , "border-color": "transparent" });
     $(".navbar").css("box-shadow", "none");
   } else if (windowScroll >= (aboutScroll + margin - windowHeight)){
-    $(".navbar-inverse").css("background-color", "#000");
+    $(".navbar-inverse").css({"background-color": "#000" , "border-color": "#000"});
     $(".navbar").css("box-shadow", "0 8px 10px 1px rgba(0, 0, 0, .14), 0 3px 14px 2px rgba(0, 0, 0, .12), 0 5px 5px -3px rgba(0, 0, 0, .2)");
   }
 });
