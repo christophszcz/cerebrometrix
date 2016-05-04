173082d2e5947a13324b3ff84c8f94e95a671204
Christoph Szczechowicz <christoph.szczechowicz@gmail.com>
2016-05-03 22:49:53 -0400

    The navbar becomes transparent when you scroll away from the main section

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
-$(document).ready(function(){
-
+$(window).scroll(function() {
+  var aboutScroll = $('#about-scroll').offset().top,
+    margin = $('#about-scroll').outerHeight(),
+    windowHeight = $(window).height(),
+    windowScroll = $(this).scrollTop();
+  if ( windowScroll < (aboutScroll + margin - windowHeight)){
+    $(".navbar-inverse").css("background-color", "transparent");
+  } else if (windowScroll >= (aboutScroll + margin - windowHeight)){
+    $(".navbar-inverse").css("background-color", "#000");
+  }
 });
