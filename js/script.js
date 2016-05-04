40d3367c87880d9bc143388cd0a6a92c105e1cdf
Christoph Szczechowicz <christoph.szczechowicz@gmail.com>
2016-05-03 22:55:18 -0400

    Removed the shadow under the navbar while in the main section

--- js/script.js
+++ js/script.js
@@ -5,7 +5,9 @@ $(window).scroll(function() {
     windowScroll = $(this).scrollTop();
   if ( windowScroll < (aboutScroll + margin - windowHeight)){
     $(".navbar-inverse").css("background-color", "transparent");
+    $(".navbar").css("box-shadow", "none");
   } else if (windowScroll >= (aboutScroll + margin - windowHeight)){
     $(".navbar-inverse").css("background-color", "#000");
+    $(".navbar").css("box-shadow", "0 8px 10px 1px rgba(0, 0, 0, .14), 0 3px 14px 2px rgba(0, 0, 0, .12), 0 5px 5px -3px rgba(0, 0, 0, .2)");
   }
 });
