ea0dfc819576d1ca286977cad8cf85c5a856f60e
Christoph Szczechowicz <christoph.szczechowicz@gmail.com>
2016-05-24 15:12:07 -0400

    Adjusted down arrow button

--- js/script.js
+++ js/script.js
@@ -59,6 +59,14 @@ $(document).ready(function(){
     }
   });
 
+  $('.btn-circle.btn-lg').hover(function(){
+    $('.glyphicon-menu-down').addClass('shadedDownArrow');
+    },
+    function(){       
+    $('.glyphicon-menu-down').removeClass('shadedDownArrow');   
+    } 
+  );
+
   $(document).on('click', '.page-scroll.text', function() { 
     $('li').removeClass('highlight');
     $('#about-link, #our-team-link, #work-link, #get-a-quote-link, #contact-link').css('color',  '#fff');
