26b6ed97033101ecc5eb65c6e7f182ad036d9287
Christoph Szczechowicz <christoph.szczechowicz@gmail.com>
2016-05-25 21:44:42 -0400

    Removed white highlight in navbar when clicking on navbar brand

--- js/script.js
+++ js/script.js
@@ -67,6 +67,11 @@ $(document).ready(function(){
     } 
   );
 
+  $(document).on('click', '.navbar-brand', function() { 
+    $('li').removeClass('highlight');
+    $('#about-link, #our-team-link, #work-link, #get-a-quote-link, #contact-link').css('color',  '#fff');
+  });
+
   $(document).on('click', '.page-scroll.text', function() { 
     $('li').removeClass('highlight');
     $('#about-link, #our-team-link, #work-link, #get-a-quote-link, #contact-link').css('color',  '#fff');
