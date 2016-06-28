902def3ceb3925aeb0f54c201be7cbf3bab2eea2
Christoph Szczechowicz <christoph.szczechowicz@gmail.com>
2016-06-27 23:04:57 -0400

    Added Rob's LinkedIn url

--- js/parallax.js
+++ js/parallax.js
@@ -1,6 +1,6 @@
-$(window).scroll(function(){
-  var windowScroll = $(this).scrollTop();
-  $('.contact-information').css({
-    'transform' : 'translateY(' + windowScroll2 + 'px)'
-  });
-});
+// $(window).scroll(function(){
+//   var windowScroll = $(this).scrollTop();
+//   $('.contact-information').css({
+//     'transform' : 'translateY(' + windowScroll + 'px)'
+//   });
+// });
