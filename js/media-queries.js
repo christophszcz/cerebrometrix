3a53fb44eaa8b6c930f47fe2fa26824824d8bdf5
Christoph Szczechowicz <christoph.szczechowicz@gmail.com>
2016-08-28 16:41:06 -0400

    My description is now mobile responsive

---
+++ js/media-queries.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    checkSize();
+    $(window).resize(checkSize);
+});
+ 
+function checkSize(){
+  if($(".chris-description").css("margin-bottom") === "40px"){
+    $(".small-before-chris").insertBefore(".small-after-chris");
+    // $(".small-before-chloe").insertBefore(".small-after-chloe");
+  } else {
+    $(".small-after-chris").insertBefore(".small-before-chris");
+  }
+}
+
