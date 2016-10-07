3349944c70865f2da9daa030901686bc278a16e6
christophszcz <christoph.szczechowicz@gmail.com>
2016-10-06 23:00:21 -0400

    Fixed the disappearing page names in the black navbar.

--- js/media-queries.js
+++ js/media-queries.js
@@ -17,17 +17,22 @@ function checkSize(){
   }
 }
 
+aboutScroll2 = $('#about-scroll').offset().top;
+margin2 = $('#about-scroll').outerHeight();
+ourTeamScroll2 = $('#our-team-scroll').offset().top;
+whatWeDoScroll2 = $('#what-we-do-scroll').offset().top;
+getAQuoteScroll2 = $('#get-a-quote-scroll').offset().top;
+contactScroll2 = $('#contact-scroll').offset().top;
+windowHeight2 = $(window).height();
+windowScroll2 = $(this).scrollTop();
+
 function checkImageSize(){ 
   if($('.navbar-brand-name').css('font-size') === '19px'){
     $('.main-section').css('background-size', '768px  522px'); 
     $(".navbar-inverse .navbar-nav>li>a").css("color", "!important #fff");  
-  } else if($('section.main-section').css('min-height') === '476px'){
-    $('#main-logo').height(100);
-    $('.main-section').css('background-size', '700px 476px');
-    $(".navbar-inverse .navbar-nav>li>a").css("color", "!important #fff");
   } else if ($('.navbar-brand-name').css('font-size') === '17px'){
     $(".navbar-inverse .navbar-nav>li>a").css("color", "!important #fff");
-  } else {
+  } else if (windowScroll2 > (aboutScroll2 + margin2 - windowHeight2) ){
     $('.main-section').css('background-size', '1366px 929px');
     $(".navbar-inverse .navbar-nav>li>a").css("color", "#000");
   }
